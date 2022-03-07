b1aeea5978f281fd64cec53b3c49766fd91da6b3
Kitt Cheung <kitt.cheung@se.experis.com>
2022-03-07 16:00:55 +0100

    added components

---
+++ Front end/src/components/LandingPage/Lobby.vue
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<template>
+<div>
+    <h1>
+        Lobby
+    </h1>
+</div>
+
+<div>
+    <div>
+        Game1
+    </div>
+    
+    <div>
+        Game2
+    </div>
+    
+    <div>
+        Game3
+    </div>
+    
+    <div>
+        Game4
+    </div>
+</div>
+
+</template>
+
+<style>
+</style>
---
+++ Front end/src/components/LandingPage/Login.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+
+<div>
+    <button>Login</button>
+    <button>Register</button>
+</div>
+
+</template>
+
+<style>
+
+</style>
