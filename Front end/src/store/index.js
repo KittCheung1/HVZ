77eaf715213d336048edd1ef4a42a49ff338b76d
Thomas Annerfeldt <thomas.annerfeldt@se.experis.com>
2022-03-11 16:47:01 +0100

    feat: added getters and setters to store

--- Front end/src/store/index.js
+++ Front end/src/store/index.js
@@ -2,11 +2,44 @@ import { createStore } from "vuex";
 
 const store = createStore({
     state:{
-        title: "Vuex Store"
+        selectedGame: {},
+        allGames: [],
+        selectedPlayer: {},
+        allPlayers: [],
+        selectedMission: {},
+        allMissions : [],
+        selectedKill: {},
+        allKills: [],
+        selectedSquad: {},
+        allSquads: []
     },
-    getters:{},
-    mutations:{},
-    actions:{}
+    getters:{
+        getSelectedGame: state => state.selectedGame,
+        getAllGames: state => state.allGames,
+        getSelectedPlayer: state => state.selectedPlayer,
+        getAllPlayers: state => state.allPlayers,
+        getSelectedMission: state => state.selectedMission,
+        getAllMissions: state => state.allMissions,
+        getSelectedKill: state => state.selectedKill,
+        getAllKills: state => state.allKills,
+        getSelectedSquad: state => state.selectedSquad,
+        getAllSquads: state => state.allSquads
+    },
+    mutations:{
+        setCurrentGame (state, payload) {state.selectedGame = payload},
+        setAllGames (state, payload) {state.allGames = payload},
+        setSelectedPlayer (state, payload) {state.selectedPlayer = payload},
+        setAllPlayers (state, payload) {state.allPlayers = payload},
+        setSelectedMission (state, payload) {state.selectedMission = payload},
+        setAllMissions (state, payload) {state.allMissions = payload},
+        setSelectedKill (state, payload) {state.selectedKill = payload},
+        setAllKills (state, payload) {state.allKills = payload},
+        setSelectedSquad (state, payload) {state.selectedSquad = payload},
+        setAllSquad (state, payload) {state.selectedSquad = payload}
+    },
+    actions:{
+
+    }
 });
 
 export default store;
