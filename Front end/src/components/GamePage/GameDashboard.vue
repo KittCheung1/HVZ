21116a280147de210b2ac1eb89e9e23bd8d7e8f4
Kitt Cheung <kitt.cheung@se.experis.com>
2022-03-14 13:48:26 +0100

    Feat:Switch between components

--- Front end/src/components/GamePage/GameDashboard.vue
+++ Front end/src/components/GamePage/GameDashboard.vue
@@ -3,72 +3,60 @@ import GameSquad from "./GameSquad.vue";
 import GameMission from "./GameMission.vue"
 import GameBiteList from "./GameBiteList.vue";
 import GameChat from "./GameChat.vue";
-import {ref} from 'vue'
+import GamePlayersList from "./GamePlayersList.vue"
+import GameMap from "./GameMap.vue"
+import { ref } from 'vue'
 
-let visibleMap = ref(false)
-let visibleSquad = ref(false)
-let visibleMission = ref(false)
+const activeComponent = ref('1')
 </script>
 
 
 <template>
     <div class="row mt-5 d-flex justify-content-left">
         <div class="col-md-3">
-            <div class="border border-solid d-flex justify-content-center" >
-
-                <div class="btn-group-vertical w-50 m-4 " role="group" data-toggle="buttons">
-                    
-                    <input
+            <div class="border border-solid d-flex justify-content-center">
+                <div class="btn-group btn-group-vertical btn-group-toggle w-75 m-4" aria-label="GameDashboard" role="group" data-toggle="buttons">
+                    <button
+                        @click="activeComponent = '1'"
                         type="radio"
-                        class="btn-check"
-                        name="btnradio"
-                        id="btnradio1"
-                        autocomplete="off"
-                        checked
-                    />
-                    <label class="btn btn-outline-primary" @click="visibleMap = !visibleMap" for="btnradio1">Map</label>
-                        <input
+                        class="btn btn-outline-primary active"
+                        aria-pressed="true"
+                    >Map</button>
+                    <button
+                        @click="activeComponent = '2'"
                         type="radio"
-                        class="btn-check"
-                        name="btnradio"
-                        id="btnradio2"
-                        autocomplete="off"
-                    />
-                    <label class="btn btn-outline-primary" @click="visibleSquad = !visibleSquad" for="btnradio2">Squads</label>
-                    <input
+                        class="btn btn-outline-primary"
+                    >Squad</button>
+                    <button
+                        @click="activeComponent = '3'"
                         type="radio"
-                        class="btn-check"
-                        name="btnradio"
-                        id="btnradio3"
-                        autocomplete="off"
-                    />
-                    <label class="btn btn-outline-primary" @click="visibleMission = !visibleMission" for="btnradio3">Missions</label>
-    
-                    <input
-                        type="radio"
-                        class="btn-check"
-                        name="btnradio"
-                        id="btnradio2"
-                        autocomplete="off"
-                    />
-                    <label class="btn btn-outline-primary" @click="visibleBiteList = !visibleBiteList" for="btnradio2">Bite List</label>
-    
-                    <input
-                        type="radio"
-                        class="btn-check"
-                        name="btnradio"
-                        id="btnradio2"
-                        autocomplete="off"
-                    />
-                    <label class="btn btn-outline-primary" @click="visibleChat = !visibleChat" for="btnradio2">Chat</label>
+                        class="btn btn-outline-primary"
+                    >Mission</button>
+                    <button
+                        @click="activeComponent = '4'"
+                        type="button"
+                        class="btn btn-outline-primary"
+                    >Bite List</button>
+                    <button
+                        @click="activeComponent = '5'"
+                        type="button"
+                        class="btn btn-outline-primary"
+                    >Players List</button>
+                    <button
+                        @click="activeComponent = '6'"
+                        type="button"
+                        class="btn btn-outline-primary"
+                    >Chat</button>
                 </div>
             </div>
         </div>
         <div class="col-md-9 border">
-            <GameSquad v-if="visibleSquad"/>
-            <GameMission v-if="visibleMission"/>
-            <GameBiteList v-if="visibleBiteList"/>
-            <GameChat v-if="visibleChat"/>
+            <GameMap v-if="activeComponent === '1'" />
+            <GameSquad v-if="activeComponent === '2'" />
+            <GameMission v-if="activeComponent === '3'" />
+            <GameBiteList v-if="activeComponent === '4'" />
+            <GamePlayersList v-if="activeComponent === '5'" />
+            <GameChat v-if="activeComponent === '6'" />
         </div>
     </div>
 </template>
@@ -77,6 +65,7 @@ let visibleMission = ref(false)
 
 
 <style>
-
+.active {
+}
 </style>
 
--- Front end/src/components/GamePage/GameSquad.vue
+++ Front end/src/components/GamePage/GameSquad.vue
@@ -1,26 +1,39 @@
 <script setup>
+
+let Squads =
+    [
+        {
+            'id': 1,
+            'name': 'Squid-Squad',
+            'players': 1,
+            'nw_lat': null,
+            'nw_lng': null,
+            'se_lat': null,
+            'se_lng': null
+        },
+        {
+            'id': 2,
+            'name': 'Squash-Squad',
+            'players': 2,
+            'nw_lat': null,
+            'nw_lng': null,
+            'se_lat': null,
+            'se_lng': null
+        }
+    ]
+
 </script>
 
 <template>
     <div class="text-center">
-        <h3>Squad Info</h3>
-        <div>
-            <h5>Squad 1</h5>
-            <p>Player 1</p>
-            <p>Player 2</p>
-            <p>Player 3</p>
-        </div>
-        <div>
-            <h5>Squad 2</h5>
-            <p>Player 1</p>
-            <p>Player 2</p>
-            <p>Player 3</p>
-        </div>
-        <div>
-            <h5>Squad 3</h5>
-            <p>Player 1</p>
-            <p>Player 2</p>
-        </div>
+        <h3>Squad Information</h3>
+        <ul v-for="gameItem in Squads" :key="gameItem">
+            <div>
+                <h6>{{ gameItem.name }}</h6>
+                <p>{{ gameItem.players }}</p>
+                <p>Test Text</p>
+            </div>
+        </ul>
     </div>
 </template>
 
